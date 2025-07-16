8ea932d1959aa6b49c65056bb2a50bc4b14093da
hannalee7222 <hannah.lee7222@gmail.com>
2025-07-16 15:35:28 +0900

    [Redux] React 15일차 과제 제출

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,14 @@
-import data from "../assets/data";
+import { useDispatch, useSelector } from 'react-redux';
+import data from '../assets/data';
+import { removeFromCart } from '../redux/redux';
 
-function Cart({ menu, cart, setCart }) {
+function Cart() {
+  const cart = useSelector((state) => state.cartReducer);
+  const menu = useSelector((state) => state.menuReducer);
   if (!menu)
     return (
-      <div style={{ textAlign: "center", margin: "80px" }}>
-        {" "}
+      <div style={{ textAlign: 'center', margin: '80px' }}>
+        {' '}
         메뉴 정보가 없어요!
       </div>
     );
@@ -16,12 +20,11 @@ function Cart({ menu, cart, setCart }) {
         {cart?.length ? (
           cart.map((el) => (
             <CartItem
-              key={el.id}
+              key={el.id + JSON.stringify(el.options)}
               item={allMenus.find((menu) => menu.id === el.id)}
               options={el.options}
               quantity={el.quantity}
-              cart={cart}
-              setCart={setCart}
+              id={el.id}
             />
           ))
         ) : (
@@ -32,7 +35,8 @@ function Cart({ menu, cart, setCart }) {
   );
 }
 
-function CartItem({ item, options, quantity, cart, setCart }) {
+function CartItem({ item, options, quantity, id }) {
+  const dispatch = useDispatch();
   return (
     <li className="cart-item">
       <div className="cart-item-info">
@@ -41,7 +45,7 @@ function CartItem({ item, options, quantity, cart, setCart }) {
       </div>
       <div className="cart-item-option">
         {Object.keys(options).map((el) => (
-          <div key={el.id}>
+          <div key={el}>
             {el} : {data.options[el][options[el]]}
           </div>
         ))}
@@ -50,7 +54,7 @@ function CartItem({ item, options, quantity, cart, setCart }) {
       <button
         className="cart-item-delete"
         onClick={() => {
-          setCart(cart.filter((el) => item.id !== el.id));
+          dispatch(removeFromCart(id));
         }}
       >
         삭제
